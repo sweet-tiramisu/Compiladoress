0205d646f8a0eaded13279295f85427ce9afa998
sweet-tiramisu <ibeatriz.lopez@um.es>
2025-04-11 23:52:11 +0200

    compiladores-sin-conflictos

---
+++ P1(1)/P1/prueba.c
@@ -0,0 +1,13 @@
+prueba(){
+const int a=0, b=0;
+var int c;
+print ("Inicio del programa\n");
+if (a) { print ("a","\n");
+ } else if (b) { print ("No a y b\n");
+      } else while (c){
+            {
+                  print ("c = ",c,"\n");
+            }
+             }
+  print ("Final","\n");
+}
---
+++ P1(1)/P1/prueba1.c
@@ -0,0 +1,11 @@
+pruebaSem(){
+var int a;
+var int a;
+const int a=0, b=0;
+
+a=5+x;
+b=5;
+c=0;
+read(a,b);
+read(c);
+}
